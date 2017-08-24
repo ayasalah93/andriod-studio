2984840ae4e22a2dbafeca9df1349a94d1dff5bd
Aya Salah <aya.salah_93@outlook.com>
2017-08-24 12:00:52 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-if [[ $(kubectl get deployments | grep $DEPLOYMENT_NAME) ]]; then
+if [[ $(kubectl get deployments | grep andriod) ]]; then
 echo "deployments exists"
 else
-kubectl run $DEPLOYMENT_NAME --image=$IMAGE_NAME --replicas=2 --output=yaml --dry-run > "deploy-rc.yaml"
+kubectl run andriod --image=$IMAGE_NAME --replicas=2 --output=yaml --dry-run > "deploy-rc.yaml"
 kubectl create -f "deploy-rc.yaml"
 fi

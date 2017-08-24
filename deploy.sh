0c579ccf818139eeb766012572eb69a0137e72a6
Aya Salah <aya.salah_93@outlook.com>
2017-08-24 15:30:09 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 if [[ $(kubectl get deployments | grep andriod) ]]; then
 echo "deployments exists"
 else
-kubectl run -i --tty andriod --port=5901 --image=manar21/android-studio:junit-fabricplugin --replicas=2 --output=yaml --restart=Never
+kubectl run -i --tty andriod --port=5901 --image=manar21/android-studio:junit-fabricplugin --replicas=1 --output=yaml --restart=Never
 fi

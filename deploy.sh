ad15ed8ea6863676b7308321db14f7b7e2b9cbfd
Aya Salah <aya.salah_93@outlook.com>
2017-08-24 11:12:47 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-if [[ $(kubectl get deployments | grep andriostudio) ]]; then
-echo "deployments exists"
-else
-kubectl run andriostudio --image=manar21/android-studio --replicas=2 --output=yaml --dry-run > "deployment-rc.yaml"
-kubectl create -f "deployment-rc.yaml"
-fi

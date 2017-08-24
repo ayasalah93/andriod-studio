27c0a5613d37379eaf5556036be95743330d5f2a
Aya Salah <aya.salah_93@outlook.com>
2017-08-24 12:16:24 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 if [[ $(kubectl get deployments | grep andriod) ]]; then
 echo "deployments exists"
 else
-kubectl run andriod --image=nginx --replicas=2 --output=yaml --dry-run > "deploy-rc.yaml"
-kubectl create -f "deploy-rc.yaml"
+kubectl run andriod --image=nginx --replicas=2 --output=yaml --dry-run > "nginxdeploy-rc.yaml"
+kubectl create -f "nginxdeploy-rc.yaml"
 fi

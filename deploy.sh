1e67ba6d77f573de879d211bc31381af5a3b17ae
Aya Salah <aya.salah_93@outlook.com>
2017-08-24 13:40:29 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 if [[ $(kubectl get deployments | grep andriod) ]]; then
 echo "deployments exists"
 else
-kubectl run andriod -i --image=manar21/android-studio:junit-fabricplugin --replicas=1 --output=yaml --attach > "andrioddeploy-rc.yaml"
+kubectl run andriod -i --port=5901 --image=manar21/android-studio:junit-fabricplugin --replicas=1 --output=yaml --attach > "andrioddeploy-rc.yaml"
 kubectl create -f "andrioddeploy-rc.yaml"
 fi
